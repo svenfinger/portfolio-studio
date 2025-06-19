2b88d17242460dd4998020203a2d9c087c053e80
svenfinger <hi@svenfinger.digital>
2025-06-20 00:02:43 +0200

    Update dependencies

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -5,5 +5,5 @@ export default defineCliConfig({
     projectId: process.env.SANITY_STUDIO_PROJECT_ID,
     dataset: process.env.SANITY_STUDIO_DATASET,
   },
-  studioHost: process.env.SANITY_STUDIO_HOSTNAME
+  studioHost: process.env.SANITY_STUDIO_HOSTNAME,
 });
