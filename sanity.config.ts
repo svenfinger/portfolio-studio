419e734ef9475b460d9736ab08791cdaa4bcd415
Sven Finger <hi@svenfinger.digital>
2025-12-07 14:25:45 +0100

    Remove visual editing

--- sanity.config.ts
+++ sanity.config.ts
@@ -4,7 +4,6 @@ import { visionTool } from '@sanity/vision';
 import { schemaTypes } from './schemaTypes';
 import { structure } from './structure';
 import { media } from 'sanity-plugin-media';
-import { presentationTool, defineDocuments, defineLocations } from 'sanity/presentation';
 
 if (!process.env.SANITY_STUDIO_PROJECT_ID) {
   throw new Error('Missing SANITY_STUDIO_PROJECT_ID');
@@ -22,49 +21,7 @@ export default defineConfig({
   mediaLibrary: {
     enabled: true,
   },
-  plugins: [
-    structureTool({ structure }),
-    visionTool(),
-    media(),
-    presentationTool({
-      previewUrl: 'http://localhost:4321',
-      resolve: {
-        mainDocuments: defineDocuments([
-          {
-            route: '/',
-            filter: '_type == "page" && isHome == true',
-          },
-          {
-            route: '/:slug',
-            filter: '_type == "page" && slug.current == $slug',
-          },
-          {
-            route: '/work/:slug',
-            filter: '_type == "work" && slug.current == $slug',
-          },
-        ]),
-        locations: {
-          page: defineLocations({
-            select: { title: 'title', slug: 'slug.current', isHome: 'isHome' },
-            resolve: (doc) => ({
-              locations: [
-                {
-                  title: doc?.title || 'Untitled',
-                  href: doc?.isHome ? '/' : `/${doc?.slug}`,
-                },
-              ],
-            }),
-          }),
-          work: defineLocations({
-            select: { title: 'title', slug: 'slug.current' },
-            resolve: (doc) => ({
-              locations: [{ title: doc?.title || 'Untitled', href: `/work/${doc?.slug}` }],
-            }),
-          }),
-        },
-      },
-    }),
-  ],
+  plugins: [structureTool({ structure }), visionTool(), media()],
   schema: {
     types: schemaTypes,
   },
